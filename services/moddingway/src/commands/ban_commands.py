bba0ed00a9b1794d949713382faeea3ea913b117
Claire <nicholas.mellinger@outlook.com>
2024-11-25 11:43:38 -0500

    Decommission modals (#190)
    Ticket: MOD-92
    
    Decommissions modals that are no longer needed.

--- services/moddingway/src/commands/ban_commands.py
+++ services/moddingway/src/commands/ban_commands.py
@@ -3,7 +3,6 @@ from discord.ext.commands import Bot
 from settings import get_settings
 from services.ban_service import ban_user
 from util import is_user_moderator
-from ui import BanModal
 from .helper import create_logging_embed, create_response_context
 
 settings = get_settings()
@@ -51,17 +50,3 @@ def create_ban_commands(bot: Bot) -> None:
                 else:
                     logging_embed.add_field(name="Error", value=error, inline=False)
                     response_message.set_string(error)
-
-    @bot.tree.context_menu(name="Ban User")
-    @discord.app_commands.check(is_user_moderator)
-    async def ban_user_action(interaction: discord.Interaction, user: discord.Member):
-        """Ban the selected user"""
-        await interaction.response.send_modal(BanModal(user))
-
-    @bot.tree.context_menu(name="Ban Message Author")
-    @discord.app_commands.check(is_user_moderator)
-    async def ban_message_author_action(
-        interaction: discord.Interaction, message: discord.Message
-    ):
-        """Ban the user that sent this message"""
-        await interaction.response.send_modal(BanModal(message.author))
