naurffxiv/moddingway

implement /delete_note
Ticket:MOD-22
/delete_note command that takes a note id then asks user for their confirmation using buttons to delete note from db.

moddingway/commands/note_commands.py
@@ -3,6 +3,36 @@ from discord.ext.commands import Bot
 from moddingway.util import is_user_moderator
 from .helper import create_logging_embed, create_response_context
 from moddingway.services import note_service
+from moddingway.database import notes_database
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class NoteDeleteView(discord.ui.View):
+    def __init__(self, note_id: int, interaction: discord.Interaction, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.note_id = note_id
+        self.original_interaction = interaction
+
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.success)
+    async def first_button_callback(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        async with create_response_context(interaction) as response_message:
+            async with create_logging_embed(
+                self.original_interaction, note_id=self.note_id
+            ) as logging_embed:
+                msg = await note_service.delete_user_note(logging_embed, self.note_id)
+
+                response_message.set_string(msg)
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.danger)
+    async def second_button_callback(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        async with create_response_context(interaction) as response_message:
+            response_message.set_string("Note deletion cancelled")
 
 
 def create_note_commands(bot: Bot) -> None:
@@ -33,3 +63,23 @@ def create_note_commands(bot: Bot) -> None:
         async with create_response_context(interaction) as response_message:
             note_details = await note_service.get_user_notes(user)
             response_message.set_string(note_details)
+
+    @bot.tree.command()
+    @discord.app_commands.check(is_user_moderator)
+    @discord.app_commands.describe(note_id="Note id to delete")
+    async def delete_note(
+        interaction: discord.Interaction,
+        note_id: int,
+    ):
+        note_row = await note_service.get_note_by_id(note_id)
+        if note_row:
+            await interaction.response.send_message(
+                f"Are you sure you want to delete this note? \n{note_row}",
+                view=NoteDeleteView(
+                    note_id=note_id, interaction=interaction, timeout=30
+                ),
+                ephemeral=True,
+            )
+
+        else:
+            await interaction.response.send_message("Note not found", ephemeral=True)

moddingway/database/notes_database.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from datetime import datetime
 from . import DatabaseConnection
 from .models import Note
 from typing import List
@@ -10,14 +9,16 @@ class Note_Display:
     note_id: int
     content: str
     created_by: int
-    last_editor: datetime
+    last_editor: int
 
 
 def from_db_row(row: tuple) -> "Note_Display":
-
-    return Note_Display(
-        note_id=row[0], content=row[1], created_by=row[2], last_editor=row[3]
-    )
+    try:
+        return Note_Display(
+            note_id=row[0], content=row[1], created_by=row[2], last_editor=row[3]
+        )
+    except:
+        return None
 
 
 def add_note(note: Note) -> int:
@@ -65,3 +66,38 @@ def list_notes(user_id: int) -> List[Note_Display]:
         rows = cursor.fetchall()
 
         return [from_db_row(row) for row in rows]
+
+
+def get_note(note_id: int) -> Note_Display:
+    conn = DatabaseConnection()
+
+    with conn.get_cursor() as cursor:
+        query = """
+        select n.noteid, n.note, n.createdby , n.lastEditedBy  
+        from notes n
+        join users u on u.userID = n.userID
+        where n.noteid = %s
+        """
+
+        params = (note_id,)
+
+        cursor.execute(query, params)
+        row = cursor.fetchone()
+
+        return from_db_row(row)
+
+
+def delete_note(note_id: int) -> bool:
+    conn = DatabaseConnection()
+
+    with conn.get_cursor() as cursor:
+        query = """
+        delete from notes n
+        where n.noteid = %s
+        """
+        params = (note_id,)
+
+        cursor.execute(query, params)
+        rows_affected = cursor.rowcount
+
+        return rows_affected > 0

naurffxiv/naur

services/moddingway/moddingway/commands/note_commands.py
@@ -3,6 +3,40 @@ from discord.ext.commands import Bot
 from moddingway.util import is_user_moderator
 from .helper import create_logging_embed, create_response_context
 from moddingway.services import note_service
+from moddingway.database import notes_database
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class NoteDeleteView(discord.ui.View):
+    def __init__(self, note_id: int, interaction: discord.Interaction, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.note_id = note_id
+        self.original_interaction = interaction
+
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.success)
+    async def confirm_button_callback(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        self.clear_items()
+        await self.original_interaction.edit_original_response(view=self)
+        async with create_response_context(interaction) as response_message:
+            async with create_logging_embed(
+                self.original_interaction, note_id=self.note_id
+            ) as logging_embed:
+                msg = await note_service.delete_user_note(logging_embed, self.note_id)
+
+                response_message.set_string(msg)
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.danger)
+    async def cancel_button_callback(
+        self, interaction: discord.Interaction, button: discord.ui.Button
+    ):
+        self.clear_items()
+        await self.original_interaction.edit_original_response(view=self)
+        async with create_response_context(interaction) as response_message:
+            response_message.set_string("Note deletion cancelled")
 
 
 def create_note_commands(bot: Bot) -> None:
@@ -33,3 +67,23 @@ def create_note_commands(bot: Bot) -> None:
         async with create_response_context(interaction) as response_message:
             note_details = await note_service.get_user_notes(user)
             response_message.set_string(note_details)
+
+    @bot.tree.command()
+    @discord.app_commands.check(is_user_moderator)
+    @discord.app_commands.describe(note_id="Note id to delete")
+    async def delete_note(
+        interaction: discord.Interaction,
+        note_id: int,
+    ):
+        note_row = await note_service.get_note_by_id(note_id)
+        if note_row:
+            await interaction.response.send_message(
+                f"Are you sure you want to delete this note? \n{note_row}",
+                view=NoteDeleteView(
+                    note_id=note_id, interaction=interaction, timeout=30
+                ),
+                ephemeral=True,
+            )
+
+        else:
+            await interaction.response.send_message("Note not found", ephemeral=True)

services/moddingway/moddingway/database/notes_database.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from datetime import datetime
 from . import DatabaseConnection
 from .models import Note
 from typing import List
@@ -10,14 +9,16 @@ class NoteDisplay:
     note_id: int
     content: str
     created_by: int
-    last_editor: datetime
+    last_editor: int
 
 
 def convert_row_to_note_display(row: tuple) -> NoteDisplay:
-
-    return NoteDisplay(
-        note_id=row[0], content=row[1], created_by=row[2], last_editor=row[3]
-    )
+    try:
+        return NoteDisplay(
+            note_id=row[0], content=row[1], created_by=row[2], last_editor=row[3]
+        )
+    except:
+        return None
 
 
 def add_note(note: Note) -> int:
@@ -65,3 +66,38 @@ def list_notes(user_id: int) -> List[NoteDisplay]:
         rows = cursor.fetchall()
 
         return [convert_row_to_note_display(row) for row in rows]
+
+
+def get_note(note_id: int) -> NoteDisplay:
+    conn = DatabaseConnection()
+
+    with conn.get_cursor() as cursor:
+        query = """
+        select n.noteid, n.note, n.createdby , n.lastEditedBy  
+        from notes n
+        join users u on u.userID = n.userID
+        where n.noteid = %s
+        """
+
+        params = (note_id,)
+
+        cursor.execute(query, params)
+        row = cursor.fetchone()
+
+        return convert_row_to_note_display(row)
+
+
+def delete_note(note_id: int) -> bool:
+    conn = DatabaseConnection()
+
+    with conn.get_cursor() as cursor:
+        query = """
+        delete from notes n
+        where n.noteid = %s
+        """
+        params = (note_id,)
+
+        cursor.execute(query, params)
+        rows_affected = cursor.rowcount
+
+        return rows_affected == 1