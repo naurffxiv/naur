b41b03e2e18cdf5c42e9370d1d1d2742a56ad62a
Alicja <alicjamahr@cohere.com>
2026-02-04 23:58:23 -0500

    feat(clearingway): create basic command handler (#122)
    * feat: create basic command handler
    
    * chore: document new env var in README
    
    * chore: handle err for InteractionRespond
    
    * chore: handle err for HandleInteraction
    
    * chore: set clearingway ci go ver to 1.25
    
    * chore: set global ci go version to 1.25
    
    * chore: convert to bulk cmd push; use library types
    
    * fix: edit on err if already responded
    
    * style: change away from one letter standard
    
    * feat: add tests for handler.go; mock discordgo.Session
    
    * feat: add tests for commands.go
    
    * chore: add run target
    
    * chore: add precommit target

--- services/clearingway/README.md
+++ services/clearingway/README.md
@@ -15,10 +15,11 @@ Clearingway is a Discord bot designed to analyse players' FFLogs data and provid
 
 ## Environment Variables
 
-| ENV                  | Description                                                          | Required | Default   |
-|----------------------|----------------------------------------------------------------------|----------|-----------|
-| CONFIG_PATH          | Relative path from project root to location of config files          | Yes      | ./configs |
-| ENV                  | The environment the bot is running in: "production" or "development" | Yes      |           |
-| DISCORD_TOKEN        | [Discord](https://discord.com/developers/docs/intro) Bot Token       | Yes      |           |
-| FFLOGS_CLIENT_ID     | Client ID for [FFLogs](https://www.fflogs.com/profile)               | Yes      |           |
-| FFLOGS_CLIENT_SECRET | Client ID for [FFLogs](https://www.fflogs.com/profile)               | Yes      |           |
+| ENV                      | Description                                                          | Required | Default   |
+|--------------------------|----------------------------------------------------------------------|----------|-----------|
+| CONFIG_PATH              | Relative path from project root to location of config files          | Yes      | ./configs |
+| ENV                      | The environment the bot is running in: "production" or "development" | Yes      |           |
+| DISCORD_TOKEN            | [Discord](https://discord.com/developers/docs/intro) Bot Token       | Yes      |           |
+| DISCORD_TESTING_GUILD_ID | Guild ID to register commands in for testing purposes only           | No       |           |
+| FFLOGS_CLIENT_ID         | Client ID for [FFLogs](https://www.fflogs.com/profile)               | Yes      |           |
+| FFLOGS_CLIENT_SECRET     | Client ID for [FFLogs](https://www.fflogs.com/profile)               | Yes      |           |
--- services/clearingway/internal/clearingway/clearingway.go
+++ services/clearingway/internal/clearingway/clearingway.go
@@ -2,33 +2,68 @@ package clearingway
 
 import (
 	"clearingway/internal/clearingway/config"
+	"clearingway/internal/commands"
 	"clearingway/internal/discord"
 	"clearingway/internal/env"
+	"log"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 type Clearingway struct {
-	Config  *config.BotConfig
-	Discord *discord.Discord
+	Config         *config.BotConfig
+	Discord        *discord.Discord
+	CommandHandler *commands.CommandHandler
 }
 
 // NewBotInstance - Initializes a new Clearingway bot instance
-func NewBotInstance(env *env.Env) (*Clearingway, error) {
+func NewBotInstance(loadedEnv *env.Env) (*Clearingway, error) {
 	// ============== LOAD CONFIG ==============
-	loadedConfig, err := config.InitBotConfig(env.CONFIG_PATH)
+	loadedConfig, err := config.InitBotConfig(loadedEnv.CONFIG_PATH)
 	if err != nil {
 		return nil, err
 	}
 
 	// ============== INITIALIZE DISCORD ==============
-	discordClient, err := discord.NewSession(env.DISCORD_TOKEN)
+	discordClient, err := discord.NewSession(loadedEnv.DISCORD_TOKEN)
 	if err != nil {
 		return nil, err
 	}
 
 	return &Clearingway{
-		Config:  loadedConfig,
-		Discord: discordClient,
+		Config:         loadedConfig,
+		Discord:        discordClient,
+		CommandHandler: initCommandHandler(discordClient.Session, loadedEnv),
 	}, nil
+}
+
+// initCommandHandler - Sets up the command handler and registers commands
+func initCommandHandler(session *discordgo.Session, loadedEnv *env.Env) *commands.CommandHandler {
+	handler := commands.NewHandler()
+
+	handler.Register(commands.PingCommand())
+
+	session.AddHandler(func(session *discordgo.Session, ready *discordgo.Ready) {
+		// If env is development, register commands to test guild only
+		// If this is a blank string, commands are registered globally
+		testingGuildId := ""
+		if loadedEnv.ENV == env.Development {
+			testingGuildId = loadedEnv.DISCORD_TEST_GUILD_ID
+		}
+
+		if err := handler.RegisterAll(session, testingGuildId); err != nil {
+			log.Printf("Error registering commands: %v", err)
+		}
+	})
+
+	session.AddHandler(func(session *discordgo.Session, inter *discordgo.InteractionCreate) {
+		err := handler.HandleInteraction(session, inter)
+		if err != nil {
+			log.Printf("Error handling interaction: %v", err)
+		}
+	})
+
+	return handler
 }
 
 // Start - Opens the Discord session
