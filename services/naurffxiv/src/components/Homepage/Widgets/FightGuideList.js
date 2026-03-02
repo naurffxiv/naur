139126665d8c38572febd7b92efedf3e3f3f8c02
khadomills <MillerNathan@cwu.edu>
2026-03-01 17:39:23 -0800

    Initial Criterion Navigation and Template Additions (#210)
    * Initial Criterion additions
    
    Added navigation for criterion on homepage, nav bar, and footer. Implemented template page for Another Merchant's Tale.
    Closes #209
    
    * Added placeholder banner

--- services/naurffxiv/src/components/Homepage/Widgets/FightGuideList.js
+++ services/naurffxiv/src/components/Homepage/Widgets/FightGuideList.js
@@ -1,10 +1,16 @@
-import { extremeList, savageList, ultimateList } from "@/config/constants";
+import {
+  extremeList,
+  savageList,
+  criterionList,
+  ultimateList,
+} from "@/config/constants";
 
 import FightGuideComponent from "./FightGuideComponent";
 
 export default function EncounterEntry() {
   const ultimateEntries = ultimateList.slice(0, 1);
   const savageEntries = savageList.slice(0, 4);
+  const criterionEntries = criterionList.slice(0, 1);
   const extremeEntries = extremeList.slice(0, 1);
 
   return (
@@ -20,6 +26,12 @@ export default function EncounterEntry() {
         entries={savageEntries}
         title="Current Savage Tier"
       />
+      {criterionEntries.length > 0 && (
+        <FightGuideComponent
+          entries={criterionEntries}
+          title="Current Criterion"
+        />
+      )}
       {extremeEntries.length > 0 && (
         <FightGuideComponent
           entries={extremeEntries}
