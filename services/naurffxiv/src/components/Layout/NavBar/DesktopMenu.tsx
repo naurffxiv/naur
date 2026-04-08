2b3a4972c6ba78b52a71aa6b0e5eb039ffd51a7a
Luna <Luna.dev.97@gmail.com>
2026-04-08 19:04:13 -0400

    refactor(naurffxiv): Layout/NavBar interactive pieces to TypeScript
    Closes #95

--- services/naurffxiv/src/components/Layout/NavBar/DesktopMenu.tsx
+++ services/naurffxiv/src/components/Layout/NavBar/DesktopMenu.tsx
@@ -1,10 +1,11 @@
 import { MenuList } from "@mui/material";
+import { type ReactElement } from "react";
 import SharedNavLinks from "./SharedNavLinks";
 
 /**
  * Desktop variant of the header navbar menu
  * */
-export function DesktopMenu() {
+export function DesktopMenu(): ReactElement {
   return (
     <MenuList sx={{ display: "flex" }}>
       <SharedNavLinks isMobile={false} />
