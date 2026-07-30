f9714555e4b85eaa7e315ce375fe0e1fea476edf
Andrew Babbitt <andrew.babbitt@outlook.com>
2026-07-30 17:03:54 -0600

    chore: add openiddict tables

---
+++ services/authingway/Data/Entities/Application.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the NAUR Contributors under one or more agreements.
+// The NAUR Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OpenIddict.EntityFrameworkCore.Models;
+
+namespace Naur.Authingway.Data.Entities;
+
+/// <summary>
+/// Represents an OpenIddict application entity with GUID identifiers.
+/// </summary>
+public class Application : OpenIddictEntityFrameworkCoreApplication<Guid, Authorization, Token>, IEntityTypeConfiguration<Application>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<Application> builder)
+    {
+        builder.ToTable("Applications");
+    }
+}
---
+++ services/authingway/Data/Entities/Authorization.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the NAUR Contributors under one or more agreements.
+// The NAUR Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OpenIddict.EntityFrameworkCore.Models;
+
+namespace Naur.Authingway.Data.Entities;
+
+/// <summary>
+/// Represents an OpenIddict authorization entity using Entity Framework Core with a GUID primary key.
+/// </summary>
+public class Authorization : OpenIddictEntityFrameworkCoreAuthorization<Guid, Application, Token>, IEntityTypeConfiguration<Authorization>
+{
+    /// <inheritdoc/>
+    public void Configure(EntityTypeBuilder<Authorization> builder)
+    {
+        builder.ToTable("Authorizations");
+    }
+}
